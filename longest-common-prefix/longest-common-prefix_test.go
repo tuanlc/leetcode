d1f77ecf1e6e334d05a5f2a84b984a355fa23343
Tuan LE CONG <tuancnttbk93@gmail.com>
2022-09-10 16:01:35 +0700

    Complete - Longest common prefix

--- longest-common-prefix/longest-common-prefix_test.go
+++ longest-common-prefix/longest-common-prefix_test.go
@@ -12,4 +12,34 @@ func TestLongestCommonPrefix(t *testing.T) {
 			t.Errorf("expected '%v' but got '%v'", expected, actual)
 		}
 	})
+
+	t.Run("should return an empty string if the input is an empty array", func(t *testing.T) {
+		input := []string{}
+		actual := longestCommonPrefix(input)
+		expected := ""
+
+		if actual != expected {
+			t.Errorf("expected '%v' but got '%v'", expected, actual)
+		}
+	})
+
+	t.Run("should return an the first string if the input has only one item", func(t *testing.T) {
+		input := []string{"car"}
+		actual := longestCommonPrefix(input)
+		expected := "car"
+
+		if actual != expected {
+			t.Errorf("expected '%v' but got '%v'", expected, actual)
+		}
+	})
+
+	t.Run("should return an the common prefix", func(t *testing.T) {
+		input := []string{"flower", "flow", "flight"}
+		actual := longestCommonPrefix(input)
+		expected := "fl"
+
+		if actual != expected {
+			t.Errorf("expected '%v' but got '%v'", expected, actual)
+		}
+	})
 }
